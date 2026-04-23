1e726e5afe0f17b992ce6137fc15945ec1195455
anwaralip06 <anwaralip06@gmail.com>
2026-04-23 11:54:33 +0700

    Menambahkan web.php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', [HomeController::class, 'dashboard'])->name('/');;
 Route::get('register', [FormController::class, 'register'])->name('register');
 Route::post('welcome', [HomeController::class, 'welcome'])->name('welcome');
 
+Route::get('saya', function () {
+    echo "Hello";
+});
+
